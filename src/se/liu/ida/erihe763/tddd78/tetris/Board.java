3dd536c562399ddc9ede0c4d743054c4e85f1c83
Eric Henziger <hemtillhenziger@gmail.com>
2014-02-19 20:47:42 +0100

    Graphical presentation works.
    Lots of unfinished methods in board.java for creating
    and moving polyominos.

--- src/se/liu/ida/erihe763/tddd78/tetris/Board.java
+++ src/se/liu/ida/erihe763/tddd78/tetris/Board.java
@@ -1,8 +1,11 @@
 package se.liu.ida.erihe763.tddd78.tetris;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.Point;
 
 /**
  * Created by erihe763 on 2014-02-16.
@@ -11,13 +14,15 @@ import java.awt.event.ActionEvent;
 public class Board {
 
     private SquareType[][] squares;
-    private Poly falling;
-    private int fallingX;
-    private int fallingY;
+    private List<BoardListener> listenerList;
+    private boolean falling;
+    private Poly fallingPoly;
+    private Point fallingPolyPos;
 
 
     public Board(int width, int height) {
         squares = new SquareType[width][height];
+        listenerList = new ArrayList<BoardListener>();
 
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
@@ -29,6 +34,7 @@ public class Board {
                 }
             }
         }
+        this.notifyListeners();
     }
 
     public int getWidth() {
@@ -45,26 +51,66 @@ public class Board {
 
     public void setSquareType(int x, int y, SquareType st) {
         this.squares[x][y] = st;
+        this.notifyListeners();
     }
 
 
     private final Action doOneStep = new AbstractAction() {
         public void actionPerformed(ActionEvent e) {
-            randomizeBoard();
+            tick();
         }
     };
+
+    private void tick() {
+        if (this.falling) {
+            this.descendPoly();
+        }
+        else {
+            this.spawnPoly();
+        }
+    }
 
     private final Timer clockTimer = new Timer(500, doOneStep);
 
 
+    public void addBoardListener(BoardListener bl) {
+        this.listenerList.add(bl);
+    }
+
+    private void notifyListeners() {
+        for (BoardListener boardListener : listenerList) {
+            boardListener.boardChanged();
+        }
+    }
     public void randomizeBoard() {
         SquareType[] types = SquareType.values();
         
-        for (int i = 0; i < this.getWidth(); i++) {
-            for (int j = 0; j < this.getHeight(); j++) {
+        for (int i = 1; i < this.getWidth() - 1; i++) {
+            for (int j = 1; j < this.getHeight() - 1; j++) {
                 this.setSquareType(i, j, types[new Random().nextInt(types.length - 1)]);
             }
         }
+
+        this.notifyListeners();
+    }
+
+    public void clearBoard() {
+        for (int i = 1; i < this.getWidth() - 1; i++) {
+            for (int j = 1; j < this.getHeight() - 1; j++) {
+                this.setSquareType(i, j, SquareType.EMPTY);
+            }
+        }
+    }
+
+    public void spawnPoly() {
+
+    }
+
+    public Point getSpawnPos() {
+        int startx = Math.round(this.getWidth() / 2);
+        int starty = this.OUTSIDE_FRAME + 1;
+        Point spawnPos = new Point(startx, starty);
+        return spawnPos;
     }
 
     public void updateBoard() {
